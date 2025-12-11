328d937b58a58a78d44a6d12120f1d39668b2a8a
Lolligerhans <>
2025-12-11 11:19:45 +0100

    Adjust

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@
 #   ./$0 100
 #   ./$0 100 100
 
-set -x
 # -c = delay up->down
 # -C = delay down->up
 # 22/23 = u/i
-sudo ./target/release/theclicker run -d"/dev/input/event2" -l22 -r23 -c"${2:-80}" -C"${1:-80}" -H
+
+echo 5: Device: CHICONY USB Keyboard-event6
+set -x
+sudo ./target/release/theclicker run -d"/dev/input/event6" -l22 -r23 -c"${2:-80}" -C"${1:-80}" -H
